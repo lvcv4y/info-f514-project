d789eb34ea5f61e04a234dc992726d948e5110e6
lvcv4y <lvcv4y@proton.me>
2026-03-25 18:32:42 +0100

    (crypto) use OOP for cryptographic functionality and keys

--- crypto.py
+++ crypto.py
@@ -4,71 +4,105 @@ Cryptography functions utils.
 TODO:
  - ZKP creation and verifications
 """
-
-def generate_vote_encryption_keys(*crypto_params):  # take G, p, g?
-    """
-    Generate (a pair of?) ElGamal public-private keys. Used for ballot encryption.
-    """
-    # TODO implement
-    # Note: Do not use ElectionAuthority. Parameters must be given in argument if needed,
-    # they are posted on the Network
-    return None, None
+from abc import ABC
 
 
-def cipher(pubkey, content):
+class AsymmetricCryptographicKey(ABC):
     """
-    Given an ElGamal public key, cipher the given content
+    Abstract class that represents a pair of crypto keys.
     """
-    # TODO implement
-    # Note: it is possible to force content to be bytes if needed.
-    #   JSON (de)serialization and string encoding could do the trick.
-    return None
+    def __init__(self, pub, private):
+        # This constructor should not be used. Keys object should be generated through child class methods.
+        self.__pub = pub
+        self.__private = private
+
+    @property
+    def public(self):
+        return self.__pub
+
+    @property
+    def private(self):
+        if self.__private is None:
+            raise ValueError("The current key is not a private key.")
+        return self.__private
+
+    def is_private(self):
+        return self.__private is not None
+
+    def as_public(self):
+        return self.__class__(self.__pub, None)
 
 
-def decipher(private_key, content):
+class VoteEncryptionKeys(AsymmetricCryptographicKey):
     """
-    Given an ElGamal private key, decipher the given content
+    ElGamal key pair, used for ballot encryption.
     """
-    # TODO implement
-    # Note: it is possible to force content to be bytes if needed.
-    #   JSON (de)serialization and string encoding could do the trick.
-    return None
+    @staticmethod
+    def generate_from(*crypto_params) -> VoteEncryptionKeys:
+        """
+        Generate a pair of ElGamal public-private keys.
+        """
+        # TODO implement
+        # Note: Do not use ElectionAuthority. Parameters must be given in argument if needed,
+        # they are posted on the Network
+        return VoteEncryptionKeys(None, None)
+
+    def product(self, k1: VoteEncryptionKeys, k2: VoteEncryptionKeys) -> VoteEncryptionKeys:
+        """
+        Compute the product of two public keys.
+        """
+        # TODO implement
+        return None
+
+    def cipher(self, content):
+        """
+        Cipher the given content.
+        """
+        # TODO implement
+        # Note: it is possible to force content to be bytes if needed.
+        #   JSON (de)serialization and string encoding could do the trick.
+        return None
+
+    def decipher(self, content):
+        """
+        Cipher the given content. The current key must be a private key.
+        """
+        # TODO implement
+        # Note: it is possible to force content to be bytes if needed.
+        #   JSON (de)serialization and string encoding could do the trick.
+        return None
 
 
-"""
-Protocols constants.
-"""
-# TODO define signature cryptographic parameters.
+class SigningKeys(AsymmetricCryptographicKey):
+    """
+    Represents a key-pair used for signature.
+    """
 
-def generate_sign_keys():
     """
-    Returns (a pair of?) public-private keys used for signature. Uses protocol constants.
+    Protocols constants.
     """
-    return None
 
+    # TODO define signature cryptographic parameters.
 
-def sign(private_key, content):
-    """
-    Given an ElGamal private key, returns the authentication signature of the given content
-    """
-    # TODO implement
-    # Note: it is possible to force content to be bytes if needed.
-    return None
+    @staticmethod
+    def generate() -> SigningKeys:
+        """
+        Generate a pair of public-private keys for signature. Uses protocol constants.
+        """
+        return SigningKeys(None, None)
 
+    def sign(self, content):
+        """
+        Sign the given content. The current key must be a private key.
+        """
+        # TODO implement
+        # Note: it is possible to force content to be bytes if needed.
+        return None
 
-def verify_signature(pubkey, signature, content) -> bool:
-    """
-    Given an ElGamal public key, returns True if the given signature is
-        an actual signature of the given content. Returns False otherwise.
-    """
-    # TODO implement
-    # Note: it is possible to force content to be bytes if needed.
-    return False
-
-
-def get_pubkey_product(pubkeys: list):
-    """
-    Given a list of ElGamal public keys on a same group G, returns their product (modulo |G|).
-        Required for bulletin distributed deciphering.
-    """
-    return None
+    def verify_signature(self, signature, content) -> bool:
+        """
+        returns True if the given signature is an actual signature of the given content. Returns False otherwise.
+        """
+        # TODO implement
+        # Note: it is possible to force content to be bytes if needed.
+        return False
