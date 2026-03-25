3ed8491b487ac2d25eb0906defce166dda3139d8
lvcv4y <lvcv4y@proton.me>
2026-03-25 18:55:28 +0100

    (crypto) add oop abstractions for content (ciphered, clear and signed)

--- crypto.py
+++ crypto.py
@@ -3,9 +3,70 @@ Cryptography functions utils.
 
 TODO:
  - ZKP creation and verifications
+
+ Note: for now, signing and ciphering are not compatible: only clear content can be signed, and signed content
+   cannot be ciphered.
 """
 from abc import ABC
 
+"""
+Content classes. Used to abstract the formats of data from their usage
+"""
+
+class CryptoContent(ABC):
+    """
+    General class, represents any cryptographic content data (signature, clear content, ciphered content).
+      The child classes will be used by the key classes to perform data conversion and signature.
+    """
+    def __init__(self, content):
+        self.__inner = content
+
+    @property
+    def inner(self):
+        return self.__inner
+
+
+class ClearContent(CryptoContent):
+    """
+    Represent some data in clear. Bridge between types and ciphered content.
+    """
+    def format(self):
+        """
+        Format the given content to an encryption-ready one (likely from any to bytes).
+        """
+        # it is (very) likely that encryption methods need a specific format, like bytes, to work.
+        #  this method is responsible for this conversion
+        # Note: JSON (de)serialization and string encoding could do be necessary.
+        # TODO implement
+        return None
+
+
+class CipheredContent(CryptoContent):
+    """
+    Represents ciphered content.
+    """
+    pass
+
+
+class Signature(CryptoContent):
+    """
+    Represents a signature.
+    """
+    pass
+
+
+class SignedContent(CryptoContent):
+    def __init__(self, content: ClearContent, signature: Signature):
+        super().__init__(content)
+        self.__signature = signature
+
+    @property
+    def signature(self):
+        return self.__signature
+
+"""
+Key classes
+"""
 
 class AsymmetricCryptographicKey(ABC):
     """
@@ -54,22 +115,18 @@ class VoteEncryptionKeys(AsymmetricCryptographicKey):
         # TODO implement
         return None
 
-    def cipher(self, content):
+    def cipher(self, content: ClearContent) -> CipheredContent:
         """
         Cipher the given content.
         """
         # TODO implement
-        # Note: it is possible to force content to be bytes if needed.
-        #   JSON (de)serialization and string encoding could do the trick.
         return None
 
-    def decipher(self, content):
+    def decipher(self, ciphered: CipheredContent) -> ClearContent:
         """
         Cipher the given content. The current key must be a private key.
         """
         # TODO implement
-        # Note: it is possible to force content to be bytes if needed.
-        #   JSON (de)serialization and string encoding could do the trick.
         return None
 
 
@@ -91,7 +148,7 @@ class SigningKeys(AsymmetricCryptographicKey):
         """
         return SigningKeys(None, None)
 
-    def sign(self, content):
+    def sign(self, content: ClearContent) -> SignedContent:
         """
         Sign the given content. The current key must be a private key.
         """
@@ -99,9 +156,9 @@ class SigningKeys(AsymmetricCryptographicKey):
         # Note: it is possible to force content to be bytes if needed.
         return None
 
-    def verify_signature(self, signature, content) -> bool:
+    def verify_signature(self, signed: SignedContent) -> bool:
         """
-        returns True if the given signature is an actual signature of the given content. Returns False otherwise.
+        returns True if the given SignedContent has a right signature. Returns False otherwise.
         """
         # TODO implement
         # Note: it is possible to force content to be bytes if needed.
