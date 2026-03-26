62f99e8026c93e700a44daf941933c4077fdb929
lvcv4y <lvcv4y@proton.me>
2026-03-26 11:50:40 +0100

    (crypto) refine oop abstractions

--- crypto.py
+++ crypto.py
@@ -5,9 +5,10 @@ TODO:
  - ZKP creation and verifications
 
  Note: for now, signing and ciphering are not compatible: only clear content can be signed, and signed content
-   cannot be ciphered.
+   cannot be ciphered. To make it possible, SignedContent must extend ClearContent.
 """
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import override
 
 """
 Content classes. Used to abstract the formats of data from their usage
@@ -18,51 +19,84 @@ class CryptoContent(ABC):
     General class, represents any cryptographic content data (signature, clear content, ciphered content).
       The child classes will be used by the key classes to perform data conversion and signature.
     """
-    def __init__(self, content):
+    @abstractmethod
+    def as_bytes(self) -> bytes:
+        """
+        Get bytes that represents the current instance content. Might be either encoded data, ciphered data, or
+            a signature.
+        """
+        pass
+
+
+class BytesContent(CryptoContent):
+    """
+    Represents any byte-encoded data.
+        Link between clear data and crypto-related operations (ciphering, signature, etc.).
+    """
+    def __init__(self, content: bytes):
+        assert isinstance(content, bytes), """
+            The inner content must be bytes, to allow cryptographic manipulations.
+            To get those bytes, either:
+                - Convert the current object directly into bytes
+                - If it's not a builtin object, extend the ClearContent interface and define the conversion methods.
+        """
         self.__inner = content
 
-    @property
-    def inner(self):
+    @override
+    def as_bytes(self) -> bytes:
         return self.__inner
 
 
-class ClearContent(CryptoContent):
+class ClearContent(ABC, CryptoContent):
     """
-    Represent some data in clear. Bridge between types and ciphered content.
+    Interface that defines the conversions between crypto-encoded data (typically bytes) and clear data (python object).
     """
-    def format(self):
+
+    # as_bytes already defined by CryptoContent
+
+    @classmethod
+    @abstractmethod
+    def from_bytes(cls, data: bytes):
         """
-        Format the given content to an encryption-ready one (likely from any to bytes).
+        Build an instance from bytes. The bytes given have the same format as the one produced by the as_bytes method.
         """
-        # it is (very) likely that encryption methods need a specific format, like bytes, to work.
-        #  this method is responsible for this conversion
-        # Note: JSON (de)serialization and string encoding could do be necessary.
-        # TODO implement
-        return None
+        pass
 
 
-class CipheredContent(CryptoContent):
+class CipheredContent(BytesContent):
     """
     Represents ciphered content.
     """
-    pass
+    def __init__(self, content: bytes, clazz: type[ClearContent]):
+        super().__init__(content)
+
+        # Saves class to be able to rebuild it once deciphered through clazz.from_bytes.
+        self.__class = clazz
+
+    @property
+    def clazz(self) -> type[ClearContent]:
+        return self.__class
 
 
-class Signature(CryptoContent):
+class Signature(BytesContent):
     """
     Represents a signature.
     """
     pass
 
 
-class SignedContent(CryptoContent):
-    def __init__(self, content: ClearContent, signature: Signature):
-        super().__init__(content)
+class SignedContent:
+    def __init__(self, data: ClearContent | BytesContent, signature: Signature):
+        self.__data = data
         self.__signature = signature
 
     @property
     def signature(self):
         return self.__signature
+
+    @property
+    def data(self):
+        return self.__data
 
 """
 Key classes
@@ -105,10 +139,11 @@ class VoteEncryptionKeys(AsymmetricCryptographicKey):
         """
         # TODO implement
         # Note: Do not use ElectionAuthority. Parameters must be given in argument if needed,
-        # they are posted on the Network
+        # they are posted on the Network.
         return VoteEncryptionKeys(None, None)
 
-    def product(self, k1: VoteEncryptionKeys, k2: VoteEncryptionKeys) -> VoteEncryptionKeys:
+    @staticmethod
+    def product(k1: VoteEncryptionKeys, k2: VoteEncryptionKeys) -> VoteEncryptionKeys:
         """
         Compute the product of two public keys.
         """
@@ -119,15 +154,17 @@ class VoteEncryptionKeys(AsymmetricCryptographicKey):
         """
         Cipher the given content.
         """
-        # TODO implement
-        return None
+        data_bytes = content.as_bytes()
+        # TODO cipher
+        return CipheredContent(data_bytes, type(content))
 
     def decipher(self, ciphered: CipheredContent) -> ClearContent:
         """
         Cipher the given content. The current key must be a private key.
         """
-        # TODO implement
-        return None
+        data_bytes = ciphered.as_bytes()
+        # TODO decipher
+        return ciphered.clazz.from_bytes(data_bytes)
 
 
 class SigningKeys(AsymmetricCryptographicKey):
@@ -152,14 +189,19 @@ class SigningKeys(AsymmetricCryptographicKey):
         """
         Sign the given content. The current key must be a private key.
         """
-        # TODO implement
-        # Note: it is possible to force content to be bytes if needed.
-        return None
+        data = content.as_bytes()
+
+        # TODO sign
+        signature = Signature(bytes())
+
+        return SignedContent(content, signature)
 
     def verify_signature(self, signed: SignedContent) -> bool:
         """
         returns True if the given SignedContent has a right signature. Returns False otherwise.
         """
-        # TODO implement
-        # Note: it is possible to force content to be bytes if needed.
+        data = signed.data.as_bytes()
+        signature = signed.signature.as_bytes()
+
+        # TODO verify signature
         return False
