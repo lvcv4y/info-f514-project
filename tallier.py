2134da807e0487e4dd3ba5ccf487dbf0b815a82a
lvcv4y <lvcv4y@proton.me>
2026-03-30 11:55:08 +0200

    (tallier) fist tallier implementation

--- tallier.py
+++ tallier.py
@@ -1,38 +1,188 @@
 """
 Tallier related objects and methods.
 """
+from typing import override
 from uuid import uuid4
 
-from crypto import SigningKeys
+from board import BBReadQuery, BBReadResult
+from crypto import SigningKeys, SignedContent, VoteEncryptionKeys, CryptoContent, CipheredContent
 from network import NetworkClient, Network, NetworkMessage
-from authorities import PKI
+from authorities import PKI, StartElectionMessage, ElectionAuthority, StopElectionMessage
+from vote import Ballot, Vote
+
+
+class TallierPartialKeyMessage(CryptoContent):
+
+    def __init__(self, tallier_id: str, pub_key: VoteEncryptionKeys, nizkp = None):
+        self.__tallier_id = tallier_id
+        self.__pub_key = pub_key
+        self.__nizkp = nizkp
+
+    @property
+    def tallier_id(self):
+        return self.__tallier_id
+
+    @property
+    def pub_key(self):
+        return self.__pub_key
+
+    @property
+    def nizkp(self):
+        return self.__nizkp
+
+    @override
+    def as_bytes(self) -> bytes:
+        tid = self.__tallier_id.encode('ascii')
+        pkey = self.__pub_key.public  # TODO is it really bytes?
+        nizkp = bytes()  # TODO manage nizkp
+
+        return tid + pkey + nizkp
+
+
+class TallierPartialDecryptionMessage(CryptoContent):
+    def __init__(self, partial_deciphered: CipheredContent, nizkps: list):
+        self.partial_deciphered = partial_deciphered
+        self.nizkps = nizkps
+
+    @override
+    def as_bytes(self) -> bytes:
+        nizkps = bytes()  # TODO manage nizkps
+        return self.partial_deciphered.as_bytes() + nizkps
 
 
 class Tallier(NetworkClient):
 
     def __init__(self, network: Network = None, pki: PKI = None):
         super().__init__()
-        self.__keys = None
         self.__network = Network() if network is None else network
         self.__pki = PKI() if pki is None else pki
 
+        self.__start_tallying = False
+        self.__bb_content = None
+
+        self.__valid_voters: list[str] | None = None
+        self.__keys: VoteEncryptionKeys | None = None
+
         self.__id = str(uuid4())
-        self.__keys = SigningKeys.generate()
-        self.__pki.add(self.__id, self.__keys.as_public())
+        self.__signing_keys = SigningKeys.generate()
+        self.__pki.add(self.__id, self.__signing_keys.as_public())
 
     @property
     def id(self):
         return self.__id
 
     def on_receive(self, message: NetworkMessage, src: NetworkClient = None):
-        # TODO implement
-        # Key generation, end of election, etc
-        pass
+        if isinstance(message, SignedContent):
+
+            if isinstance(message.data, StartElectionMessage):
+                inner: StartElectionMessage = message.data
+                auth_keys = PKI().get_key_from_client(ElectionAuthority().id)
+                if auth_keys.verify_signature(message):
+                    # Generate keys
+                    self.__keys = VoteEncryptionKeys(inner.crypto_parameters)
+                    self.__valid_voters = inner.voters
+
+                    # TODO generate nizkp
+                    nizkp = None
+
+                    reply = TallierPartialKeyMessage(self.__id, self.__keys.as_public(), nizkp)
+                    self.__network.send(
+                        self.__signing_keys.sign(reply),
+                        self,
+                        None  # Broadcast
+                    )
+
+            if isinstance(message.data, StopElectionMessage):
+                auth_keys = PKI().get_key_from_client(ElectionAuthority().id)
+
+                if auth_keys.verify_signature(message):
+                    # Enter tallying process
+                    self.__start_tallying = True
+
+                    # Fetch the BB content
+                    self.__network.send(
+                        BBReadQuery(),
+                        self,
+                        None  # Broadcast (for now)
+                    )
+
+        if isinstance(message, BBReadResult):  # For now, unsigned (TODO ?)
+            if not self.__start_tallying:
+                # We actually didn't ask this
+                return
+
+            self.__bb_content = message.state
+            self.tally()
+
 
     def tally(self):
         """
         Start tallying process.
         """
-        # TODO implement
-        pass
+
+        # Note: self.__bb_content contains the last received BB content. Might send another BBReadQuery if not complete.
+
+        # First step: get valid votes
+        valid_votes = {}
+
+        for msg in self.__bb_content:
+            obj = msg.content
+            if not isinstance(obj, SignedContent):  # Any message we consider "valid" are signed
+                continue
+
+            if isinstance(obj.data, StopElectionMessage):
+                if self.__pki.get_key_from_client(ElectionAuthority().id).verify_signature(obj):  # Valid signature
+                    break
+                else:
+                    continue
+
+            if not isinstance(obj.data, Ballot):
+                continue
+
+            ballot: Ballot = obj.data
+            if (
+                ballot.voter_id not in self.__valid_voters or  # Unknown voter
+                not self.__pki.get_key_from_client(ballot.voter_id).verify_signature(obj)  # Wrong signature
+            ):
+                continue
+
+            # TODO verify ballot nizkp
+
+            # The voter ID is legit, the signature and NIZKP are verified.
+
+            # Check duplicates
+            if ballot.voter_id in valid_votes.keys():
+                continue
+
+            # Weeding: prevent replay attack (could jeopardize vote privacy)
+            if ballot.vote_cipher in valid_votes.values():
+                continue
+
+            valid_votes[ballot.voter_id] =  ballot.vote_cipher
+
+        if len(valid_votes) != self.__valid_voters:
+            # Some votes are missing
+            # TODO error message on network?
+            raise ValueError("Missing votes.")
+
+        # Aggregate, partial decipher and post.
+
+        # TODO aggregate
+        aggregate = bytes()
+
+        partial_decipher = CipheredContent(
+            self.__keys.raw_decipher(aggregate),
+            Vote,  # TODO not really a vote, that's an aggregation
+        )
+
+        # TODO generate NIZKPs
+        nizkps = []
+
+        msg = TallierPartialDecryptionMessage(partial_decipher, nizkps)
+        self.__network.send(
+            self.__signing_keys.sign(msg),
+            self,
+            None  # Broadcast
+        )
+        self.__start_tallying = False
 
